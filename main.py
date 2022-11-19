19fae95f7b04ac31464fc09abc3ba33db6e89da3
YisehakBelay <112198008+YisehakBelay@users.noreply.github.com>
2022-11-19 02:10:25 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+I am trying to do my first python project on creating a program that let's a user to make an appointment
+for a bus where the buses have their own routes. The route of the bus contains the routing number (which means
+if the there are "n" number of buses then there are routing numbers starting from 1 to n),the departure address,
+and one address that it is passing through, the arrival address and the distance from the departure to arrival. The 
+bus contains plate number and driver's name. 
+
+In this program users can appointSeat and the program contains a file that calculates a free seat and let the user to 
+assign a seat(the assigned seat will not be available for another user). 
+
+The other file allows the user to schedule a bus based on the data of the bus(plate number and driver's name(opt))and the 
+route of the bus(routing number,departure,passing through and arriving address)
+'''
+
+
---
+++ mass.py
@@ -0,0 +1,12 @@
+'''
+
+'''
+
+def kilogramtoGram(mass):
+	converted = mass*0.0001
+	return converted
+	
+def gramtoKilogram(mass):
+	converted = mass*100
+	return converted
+	
